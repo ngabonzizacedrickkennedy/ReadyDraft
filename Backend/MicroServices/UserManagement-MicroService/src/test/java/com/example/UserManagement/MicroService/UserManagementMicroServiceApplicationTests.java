fcdd56c6eff06551c9a2e26bb326e2d44f33e691
ngabonzizacedrickkennedy <cedzlankee42@gmail.com>
2026-02-15 03:23:35 +0200

    initialization

---
+++ Backend/MicroServices/UserManagement-MicroService/src/test/java/com/example/UserManagement/MicroService/UserManagementMicroServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.UserManagement.MicroService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserManagementMicroServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
